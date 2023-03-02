4b70e9392c936ffe877961441923007cc6b69223
Franck-emerson <franckemerson1@gmail.com>
2023-03-03 00:20:19 +0100

    ajout de quelques fonctionnalités sup

--- liste_location.py
+++ liste_location.py
@@ -1,7 +1,16 @@
 '''
     ce programme permet dde calculer la caution, retour sur
     investissement initiale et temps avant 100% retour sur investissement
+
 '''
+
+import os
+
+
+dest = os.path.abspath(os.path.join(os.getcwd(), "Desktop/projet_immobilier/logiciel_python"))
+chemi = os.path.abspath(os.path.join(os.getcwd(), "Desktop/projet_immobilier/logiciel_python"))
+chemin = os.path.join(chemi, "loyer.txt")
+destination = os.path.join(dest, "tableau.txt")
 
 def immobilier_startup_rent(liste_loyer_mensuel : list):
 
@@ -12,7 +21,12 @@ def immobilier_startup_rent(liste_loyer_mensuel : list):
     revenu_sur_annee = []
     apres_2_premiers_mois = []
     mois_pour_recup_investissement = []
+    caution_total_a_payer = 0
+    donnees = ["caution", "marge", "revenu_an"]
+    liste_dictionnaires = {}
+    locataire = []
     caution = 4
+
 
     for loyer in liste_loyer_mensuel:
         cautions = loyer * caution
@@ -34,25 +48,40 @@ def immobilier_startup_rent(liste_loyer_mensuel : list):
         apres_2_mois = int(caution_a_payer[i] - les_2_premiers_mois[i])
         apres_2_premiers_mois.append(apres_2_mois)
 
+        caution_total_a_payer += caution_a_payer[i] #Caution à débouser pour tous les loyers
+
     for i in range(len(liste_loyer_mensuel)):
         mois_recup_investissement = int(apres_2_premiers_mois[i] / notre_marge[i])
         mois_pour_recup_investissement.append(mois_recup_investissement)
 
-    print("Revenu sur un An: {}".format(revenu_sur_annee))
-    print("Les cautions à payer: {}".format(caution_a_payer))
-    print("Les cautions apres 2 premier mois: {}".format(apres_2_premiers_mois))
-    print("Notre marge de 10%: {}".format(notre_marge))
-    print("Les 2 premiers mois: {}".format(les_2_premiers_mois))
-    print("Les 10 mois suivants: {}".format(les_10_mois_suivant))
-    print("Recuperer investissement: {}".format(mois_pour_recup_investissement))
+    for i in range(len(liste_loyer_mensuel)):
+        liste_dictionnaires = {donnees[0]:caution_a_payer, donnees[1]: notre_marge, donnees[2]: revenu_sur_annee}
+    locataire.append(liste_dictionnaires)
+
+    # Ouvrir le fichier en mode écriture
+    with open(destination, "w") as f:
+    # Imprimer les entêtes
+        f.write("caution\tmarge\trevenu_annuel\n")
+    # Imprimer les données
+        for i in range(len(locataire[0]['caution'])):
+            f.write(str(locataire[0]['caution'][i]) + "\t" + str(locataire[0]['marge'][i]) + "\t" + str(locataire[0]['revenu_an'][i]) + "\n")
 
     
 
+    #print(f"Caution totale: {caution_total_a_payer} frcfa")
+
 
 def main():
-    liste_loyer = []
-    for i in range(4):
-        liste_loyer.append(int(input("Entrer les différents loyer: ")))
+    
+    with open(chemin, "r") as f:
+    # lire toutes les lignes du fichier et stocker chaque ligne dans une liste
+        lignes = f.readlines()
+        liste_loyer = []
+
+    for ligne in lignes:
+    # utiliser la méthode strip() pour supprimer les caractères de saut de ligne (\n) à la fin de chaque ligne
+        chiffres_ligne = [int(chiffre) for chiffre in ligne.strip().split()]
+        liste_loyer.extend(chiffres_ligne)
     
     immobilier_startup_rent(liste_loyer)
 
